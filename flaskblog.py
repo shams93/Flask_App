c301486a38a2e6669fa90a32a7e8367487c782f0
Shams Ulzaman <shamsulzaman.s@gmail.com>
2019-01-16 23:34:03 -0330

    posted content and looped throug it

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,36 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author':'Shams Ul Zaman',
+        'title':' Post One',
+        'content':'first post contant',
+        'date_posted':'April 20, 2015',
+
+    },
+    {
+        'author':'Jane',
+        'title':' Post Two',
+        'content':'second post contant',
+        'date_posted':'April 21, 2015',
+
+    }
+
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
