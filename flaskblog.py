7d6a7552bff89293e927810f76c2aa40c856d800
Shams Ulzaman <suz250@mun.ca>
2019-01-29 21:45:26 -0330

    created forms and fixed register.html

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from form import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'c997ee3b0c2f462ac8a8916dde6ca11c'
 
 posts = [
     {
@@ -31,10 +34,20 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template('contact.html', title='Contact')
-@app.route("/registration")
-@app.route("/register")
+
+
+@app.route("/register", methods=['Get','Post'])
 def register():
-    return render_template('register.html', title='registration')
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='registration', form = form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='registration', form = form)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- form.py
+++ form.py
@@ -1,5 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[])    
+    username = StringField('Username', validators=[DataRequired(),Length(min=2,max=20)])    
+    email = StringField("User Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField("Conform Password", validators=[DataRequired(),
+    EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    """username = StringField('Username', validators=[DataRequired(),Length(min=2,max=20)])"""    
+    email = StringField("User Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remeber = BooleanField("Remember Me")
+    submit = SubmitField('Sign Up')
