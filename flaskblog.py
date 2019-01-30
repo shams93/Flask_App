9d02549c787c48ccd9c58797426b174aa331565d
Shams Ulzaman <suz250@mun.ca>
2019-01-30 20:21:59 -0330

    worked on layout html and created db

--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,30 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from form import RegistrationForm, LoginForm
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'c997ee3b0c2f462ac8a8916dde6ca11c'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db =SQLAlchemy(app)
+
+
+class user(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), unique = True, nullable = False)
+    email = db.Column(db.String(120), unique = True, nullable = False)
+    image_file = db.Column(db.String(20), nullable = False, default ='default.jpg')
+    password = db.Column(db.String(60), nullable = False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}'',)"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Colum() 
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}'',)"
+     
 
 posts = [
     {
